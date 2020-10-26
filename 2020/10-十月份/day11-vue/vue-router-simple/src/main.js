3e0f7b4788e2e297b4fabafb1a06e6c7b7f54180
caoxijuan <2357569721@qq.com>
2020-10-26 08:20:50 +0800

    提交

--- 2020/10-十月份/day11-vue/vue-router-simple/src/main.js
+++ 2020/10-十月份/day11-vue/vue-router-simple/src/main.js
@@ -1,10 +1,7 @@
 import { createApp } from "vue";
-// import { createApp } from "vue";
 import App from "./App.vue";
 // 导入路由
 import routers from "./routers";
-const app = createApp(App);
-
-// 挂载路由
-app.use(routers);
-app.mount("#app");
+const app = createApp(App)
+app.use(routers)
+app.mount('#app')
--- 2020/10-十月份/day11-vue/vue-router-simple/src/routers/index.js
+++ 2020/10-十月份/day11-vue/vue-router-simple/src/routers/index.js
@@ -1,17 +1,18 @@
 // 引入
 import { createRouter, createWebHashHistory } from "vue-router";
-import Home from "../components/Home";
+// 引入组件
 import About from "../components/About";
+import Home from "../components/Home";
 // 定义路由
-const routes = [
-  { path: "/", component: Home },
-  { path: "/about", component: About }
-];
-
+const router = [
+  { path: '/', component: About },
+  { path: '/home', component: Home }
+]
 // 创建路由
-const router = createRouter({
+const routers = createRouter({
   history: createWebHashHistory(),
   // 定义好的路由
-  routes
-});
-export default router;
+  router
+})
+// 导出路由
+export default routers
