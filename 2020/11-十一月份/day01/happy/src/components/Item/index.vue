42e947e12252a809a97398897ca6780d99a4b668
caoxijuan <2357569721@qq.com>
2020-11-02 23:41:06 +0800

    提交

---
+++ 2020/11-十一月份/day01/happy/src/components/Item/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>item</h1>
+  </div>
+</template>
+
+<script>
+// import {mapActions} from "vuex"
+export default {
+  created() {
+    this.$store.dispatch("getData");
+  }
+};
+</script>
+
+<style></style>
