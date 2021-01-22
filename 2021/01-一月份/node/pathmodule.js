15fbfd4049ad5e35422729187dea74afda38214c
caoxijuan <2357569721@qq.com>
2021-01-22 22:05:15 +0800

    提交

--- 2021/01-一月份/node/pathmodule.js
+++ 2021/01-一月份/node/pathmodule.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 const newPath = path.join('a/b/c', '\de\f')
-// console.log(newPath)
+console.log(newPath)
 
 const basename = path.basename('a/b/c/name.txt', '.txt')
 console.log(basename)
