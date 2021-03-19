2c10a864434493e96fd324fa632c38b38a6549e8
caoxijuan <2357569721@qq.com>
2021-03-19 08:38:45 +0800

    提交

---
+++ 2021/03-三月份/day06-vue/aa/vite-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<div>
+		<img alt="Vue logo" src="./assets/logo.png" />
+		<aButton :aName="name"></aButton>
+		<p>{{ msg }}</p>
+	</div>
+</template>
+
+<script>
+import aButton from "./components/aa.vue";
+export default {
+	data() {
+		return {
+			msg: "你好",
+			name: "小白",
+		};
+	},
+	methods: {},
+	components: {
+		aButton,
+	},
+};
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
---
+++ 2021/03-三月份/day06-vue/aa/vite-project/src/components/aa.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<h1>哈哈哈</h1>
+		<p>{{ aName }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ["aName"],
+};
+</script>
+
+<style>
+</style>
